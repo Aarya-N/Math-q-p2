4d64cebbd425617529952590c1643298aade83c5
Aarya-N <145162169+Aarya-N@users.noreply.github.com>
2023-12-17 00:44:41 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,17 @@
+// Create function addUser()
+function ____ {
+  // Get value of user by id player1_name_input and player2_name_input
+
+  // Store these values locally
+
+  //Assign "game_page.html" to window.location
+}
+function addUser() {
+  document.getElementById("player1_name_input");
+  document.getElementById("player1_name_input");
+
+  localStorage.setItem("player1_name_input");
+  localStorage.setItem("player1_name_input");
+
+  window.location = "game_page.html";
+}
